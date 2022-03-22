b9506b64c2e9459377a2bb9aeb4813931f12c3d1
unknown <kev972@gmail.com>
2022-03-22 19:39:30 +0100

    dev historique stock change

--- src/components/historiqueStock/index.js
+++ src/components/historiqueStock/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-let HistoriqueStock =({role,env})=>{
+let HistoriqueStock =({role,env,connect})=>{
     let navigate = useNavigate();
    const [historiqueData , setHistoriqueData] = useState([]);
    let baseUrlProd = "http://3.145.43.146:9001"
